e45075d293d936d5eb22400891da36f0301e873c
Khushhal <khushalgupta.98@gmail.com>
2022-04-11 14:01:33 +0530

    Adding workflow

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "passbirdappd0bb6b57d0bb6b57": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
